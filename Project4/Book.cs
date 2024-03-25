413ace795bb31a2a6d627e2f2531f5587b24ee46
jp-eustis <112917977+jp-eustis@users.noreply.github.com>
2024-03-25 17:13:08 -0400

    Add files via upload

---
+++ Project4/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class Book : Product
+    {
+        public string Author { get; set; } = "";
+
+        public Book() { }
+        public Book(string code, string description, string price, string author) : base(code, description, price) => 
+            Author = author;
+
+        public override string GetDisplayText(string sep) => $"Book{sep}{Code}{sep}{Description}{sep}{Price}{sep}{Author}";
+    }
+}
---
+++ Project4/Software.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class Software : Product
+    {
+        public string Version { get; set; } = "";
+
+        public Software() { }
+        public Software(string code, string description, string price, string version) : base(code, description, price) => 
+            Version = version;
+
+        public override string GetDisplayText(string sep) => $"Software{sep}{Code}{sep}{Description}{sep}{Price}{sep}{Version}";
+    }
+}
